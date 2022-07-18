6d34917ee88f88d728217f18099ffa916931f624
GianGullaci <gullacigianfranco@gmail.com>
2022-07-17 23:49:25 -0300

    Creando bloque1

---
+++ home/bloque1.php
@@ -0,0 +1,61 @@
+<main>
+<section class="pt-4 pb-0 card-grid">
+    <div class="container">
+        <div class="row g-4">
+            <div class="col-lg-6 col-xs-12">
+                <div class="card">
+                    <img src="img/modulos/m 4.jpg" class="img-fluid rounded-start" alt="...">
+                    <div class="card-img-overlay d-flex align-items-center p-3 p-sm-4">
+                        <div class="w-100 mt-auto">
+                            <a href="" class="badge bg-dark mb-2">
+                                <font style="vertical-align: inherit;">La Jornada que Pasó</font>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 col-xs-12">
+                <div class="row g-4">
+                    <div class="col-lg-6 col-xs-12">
+                        <div class="card">
+                            <img src="img/modulos/m 4.jpg" class="img-fluid rounded-start" alt="...">
+                            <div class="card-img-overlay d-flex align-items-center p-3 p-sm-4">
+                                <div class="w-100 mt-auto">
+                                <a href="" class="badge bg-dark mb-2">
+                                    <font style="vertical-align: inherit;">La Jornada que Pasó</font>
+                                </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-xs-12">
+                        <div class="card">
+                            <img src="img/modulos/m 4.jpg" class="img-fluid rounded-start" alt="...">
+                            <div class="card-img-overlay d-flex align-items-center p-3 p-sm-4">
+                                <div class="w-100 mt-auto">
+                                <a href="" class="badge bg-dark mb-2">
+                                    <font style="vertical-align: inherit;">La Jornada que Pasó</font>
+                                </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-12 col-xs-12">
+                        <div class="card">
+                            <img src="img/modulos/m2 h.jpg" class="img-fluid rounded-start" alt="...">
+                            <div class="card-img-overlay d-flex align-items-center p-3 p-sm-4">
+                                <div class="w-100 mt-auto">
+                                <a href="" class="badge bg-dark mb-2">
+                                    <font style="vertical-align: inherit;">La Jornada que Pasó</font>
+                                </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+</main>
--- template/header.php
+++ template/header.php
@@ -7,7 +7,9 @@
 
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+    <!--<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">-->
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.1/font/bootstrap-icons.css">
 
