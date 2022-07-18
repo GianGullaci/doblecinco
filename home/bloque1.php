35d14a0b81cfce6a19608055f7f76cf572faf2c2
GianGullaci <gullacigianfranco@gmail.com>
2022-07-18 11:20:07 -0300

    actualizado

--- home/bloque1.php
+++ home/bloque1.php
@@ -2,52 +2,68 @@
 <section class="pt-4 pb-0 card-grid">
     <div class="container">
         <div class="row g-4">
-            <div class="col-lg-6 col-xs-12">
-                <div class="card">
-                    <img src="img/modulos/m 4.jpg" class="img-fluid rounded-start" alt="...">
+            <div class="col-lg-6">
+                <div class="card" style="background-image:url('img/modulos/m 4.jpg'); background-position:center left;background-size:cover;overflow:hidden;z-index:99;will-change:transform;height:560px;">
                     <div class="card-img-overlay d-flex align-items-center p-3 p-sm-4">
                         <div class="w-100 mt-auto">
-                            <a href="" class="badge bg-dark mb-2">
+                            <a href="" class="badge bg-secondary bg-gradient mb-2">
                                 <font style="vertical-align: inherit;">La Jornada que Pasó</font>
                             </a>
+                            <h2 class="text-white fw-bolder">
+                                <a href="" class="stretched-link text-reset badge bg-secondary bg-gradient">
+                                    <font style="vertical-align: inherit;">El Gallego venció la muralla de Dublin</font>  
+                                </a>
+                            </h2>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-6 col-xs-12">
+            <div class="col-lg-6">
                 <div class="row g-4">
-                    <div class="col-lg-6 col-xs-12">
-                        <div class="card">
-                            <img src="img/modulos/m 4.jpg" class="img-fluid rounded-start" alt="...">
+                    <div class="col-12">
+                        <div class="card" style="background-image:url('img/modulos/m2 h.jpg'); background-position:center left;background-size:cover;overflow:hidden;z-index:99;will-change:transform;height:calc(280px - calc(var(--bs-gutter-y) / 2));">
                             <div class="card-img-overlay d-flex align-items-center p-3 p-sm-4">
                                 <div class="w-100 mt-auto">
-                                <a href="" class="badge bg-dark mb-2">
-                                    <font style="vertical-align: inherit;">La Jornada que Pasó</font>
-                                </a>
+                                    <a href="" class="badge bg-secondary bg-gradient mb-2">
+                                        <font style="vertical-align: inherit;">La Jornada que Pasó</font>
+                                    </a>
+                                    <h4 class="text-white fw-bolder">
+                                        <a href="" class="stretched-link text-reset badge bg-secondary bg-gradient">
+                                            <font style="vertical-align: inherit;">El Gallego venció la muralla de Dublin</font>  
+                                        </a>
+                                    </h4>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-6 col-xs-12">
-                        <div class="card">
-                            <img src="img/modulos/m 4.jpg" class="img-fluid rounded-start" alt="...">
+                    <div class="col-md-6">
+                        <div class="card" style="background-image:url('img/modulos/m 4.jpg'); background-position:center left;background-size:cover;overflow:hidden;z-index:99;will-change:transform;height:calc(280px - calc(var(--bs-gutter-y) / 2));">
                             <div class="card-img-overlay d-flex align-items-center p-3 p-sm-4">
                                 <div class="w-100 mt-auto">
-                                <a href="" class="badge bg-dark mb-2">
-                                    <font style="vertical-align: inherit;">La Jornada que Pasó</font>
-                                </a>
+                                <a href="" class="badge bg-secondary bg-gradient mb-2">
+                                        <font style="vertical-align: inherit;">La Jornada que Pasó</font>
+                                    </a>
+                                    <h4 class="text-white fw-bolder">
+                                        <a href="" class="stretched-link text-reset badge bg-secondary bg-gradient" style="white-space: initial; overflow: hidden;text-align:left;">
+                                            <font style="vertical-align: inherit;">El Gallego venció la muralla de Dublin</font>  
+                                        </a>
+                                    </h4>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-12 col-xs-12">
-                        <div class="card">
-                            <img src="img/modulos/m2 h.jpg" class="img-fluid rounded-start" alt="...">
+                    <div class="col-md-6">
+                        <div class="card" style="background-image:url('img/modulos/m 4.jpg'); background-position:center left;background-size:cover;overflow:hidden;z-index:99;will-change:transform;height:calc(280px - calc(var(--bs-gutter-y) / 2));">
                             <div class="card-img-overlay d-flex align-items-center p-3 p-sm-4">
                                 <div class="w-100 mt-auto">
-                                <a href="" class="badge bg-dark mb-2">
-                                    <font style="vertical-align: inherit;">La Jornada que Pasó</font>
-                                </a>
+                                    <a href="" class="badge bg-secondary bg-gradient mb-2">
+                                        <font style="vertical-align: inherit;">La Jornada que Pasó</font>
+                                    </a>
+                                    <h4 class="text-white fw-bolder">
+                                        <a href="" class="stretched-link text-reset badge bg-secondary bg-gradient" style="white-space: initial; overflow: hidden;text-align:left;">
+                                            <font style="vertical-align: inherit;">El Gallego venció la muralla de Dublin</font>  
+                                        </a>
+                                    </h4>
                                 </div>
                             </div>
                         </div>
