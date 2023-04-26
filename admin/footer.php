90f90162e633053806d7f3ad4ab8e535b1dd8b69
GianGullaci <gullacigianfranco@gmail.com>
2023-04-26 13:42:35 -0300

    Ignore

--- admin/footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-
-	<p>
-		<span style="text-align:left;float:left">&copy; 2015 <a href="http://invixion.com" alt="INvixion">INvixion</a></span>
-		
-	</p>
-
-</footer>
--- admin/includes/started.php
+++
@@ -1,6 +0,0 @@
-<?php  session_start();
-	if (isset($_SESSION['admin']) and $_SESSION['admin']==1){
-		header("Location: index.php");
-	}
-    
-?>
